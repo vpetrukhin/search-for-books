04818623df136bac57916c44f744745e1f5eb762
Vasya <vpetruin@inbox.ru>
2021-08-17 20:44:52 +0300

    сверстан компонентSearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,14 +7,21 @@ const SearchForm = () => {
 
     console.log('submit');
   }
+
+  const selectHandler = (e) => {
+    console.log(e.target.value);
+  }
   
   return (
     <form className="search-form" onSubmit={submitHandler}>
-      <input type="text" className="search-form__input"/>
+      <div className="search-form__inner">
+        <input type="text" className="search-form__input"/>
+        <button type="submit" className="search-form__btn" />
+      </div>
       <div className="search-form__categories-wrapper">
         <label htmlFor="categories" className="search-form__categories-label">Categories</label>
-        <select name="categories" id="categories" className="search-form__categories">
-          <option value="all" className="search-form__category">all</option>
+        <select name="categories" id="categories" className="search-form__categories" onChange={selectHandler}>
+          <option value="all" className="search-form__category" selected>all</option>
           <option value="art" className="search-form__category">art</option>
           <option value="biogrphy" className="search-form__category">biogrphy</option>
           <option value="computers" className="search-form__category">computers</option>
@@ -24,9 +31,9 @@ const SearchForm = () => {
         </select>
       </div>
       <div className="search-form__filter-wrapper">
-        <label htmlFor="#" className="search-form__filter-label">Sorting by</label>
-        <select name="filter" id="filter" className="search-form__filter">
-          <option value="relevanse" className="search-form__filter-option">relevance</option>
+        <label htmlFor="filter" className="search-form__filter-label">Sorting by</label>
+        <select name="filter" id="filter" className="search-form__filter" defaultValue="relevanse">
+          <option value="relevanse" className="search-form__filter-option" selected>relevance</option>
           <option value="newest" className="search-form__filter-option">newest</option>
         </select>
       </div>
